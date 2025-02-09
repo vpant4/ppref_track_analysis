a9b6e147f5e53ec373566c305bb97ff89c67df48
vpant4 <vpant@lxplus973.cern.ch>
2025-02-09 22:52:52 +0100

    Added jet stuff

--- Track_Analyzer.C
+++ Track_Analyzer.C
@@ -1,5 +1,5 @@
 #include "include_libraries.h" //header file with important libraries
-
+#include "read_trees.h"
 
 
 void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t pthat_value)
@@ -48,6 +48,11 @@ void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t
       trk_tree->Add(*listIterator);	    
 
     }
+
+    hlt_tree->AddFriend(trk_tree);
+    hlt_tree->AddFriend(hea_tree);
+    hlt_tree->AddFriend(jet_tree);
+    hlt_tree->AddFriend(ski_tree);
   	
 
     //Define the relevant histograms*********************************************
@@ -62,16 +67,19 @@ void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t
     TH1D* hpthatW = new TH1D("hpthatW", "", 200, 0, 600.);
     TH1D* hZvtx = new TH1D("hZvtx", "", 80, -20, 20);
 
-
     // 1-d and 2-d histograms to store trk info
     TH2D* htrkpteta = new TH2D("htrkpteta","htrkpteta",1000,0.,500.,50,-1.,1.);
     TH1D* hinvyield = new TH1D("hinvyield","hinvyield",nptbins,pTbins);
     TH1D* hNtrk     = new TH1D("hNtrk","hNtrk",nptbins,pTbins);
-    
+
+    //Histograms to store jet information
+    TH1D *hjtpt     = new TH1D("hjtpt","hjtpt",1000,0.,1000.);
+    TH1D *hjteta    = new TH1D("hjteta","hjteta",1000,-2.,2.);
+    TH1D *hjtphi    = new TH1D("hjtphi","hjtphi",1000,-TMath::Pi(),TMath::Pi());
 
     
         
-    int nevents = hea_tree->GetEntries(); // number of events
+    int nevents = hlt_tree->GetEntries(); // number of events
     cout << "Total number of events in those files: "<< nevents << endl;
     cout << endl;
     cout << "-------------------------------------------------" << endl;
@@ -84,130 +92,13 @@ void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t
 	if (i%100000==0) cout<<i<<" events passed "<<endl;    
 	
 
-	//Event information******************************************************
+	//Read trees******************************************************
 
-	hea_tree->SetBranchStatus("*", 0);
-	Float_t vz;
-	Float_t weight;
-	Float_t pthat;
-
-	hea_tree->SetBranchStatus("vz", 1);
-	hea_tree->SetBranchAddress("vz", &vz);
-
-	if(is_MC)
-	  {
-	    hea_tree->SetBranchStatus("weight", 1);
-	    hea_tree->SetBranchAddress("weight", &weight);
-	  
-	    hea_tree->SetBranchStatus("pthat", 1);
-	    hea_tree->SetBranchAddress("pthat", &pthat);
-	  } 
-	    
-	//HLT information*********************************************************
-	hlt_tree->SetBranchStatus("*", 0);
-
-	Int_t HLT_AK4PFJet120_v8; //Flag for Jet120 trigger
-	Int_t HLT_ZeroBias_v13;   //Flag for zero bias trigger
-      
-	hlt_tree->SetBranchStatus("HLT_ZeroBias_v13", 1);
-        hlt_tree->SetBranchAddress("HLT_ZeroBias_v13", &HLT_ZeroBias_v13);
-
-	hlt_tree->SetBranchStatus("HLT_AK4PFJet120_v8", 1);
-        hlt_tree->SetBranchAddress("HLT_AK4PFJet120_v8", &HLT_AK4PFJet120_v8);
-
-	//Skim tree information***************************************************
-
-	ski_tree->SetBranchStatus("*", 0);
-
-	Int_t HBHENoiseFilterResultRun2Loose;
-	Int_t pprimaryVertexFilter;
-	Int_t pBeamScrapingFilter;
-	Int_t phfCoincFilter;
-	Int_t pVertexFilterCutdz1p0;
-	Int_t pVertexFilterCutGplus;
-	Int_t pVertexFilterCutVtx1;
-
-	//	ski_tree->SetBranchStatus("HBHENoiseFilterResultRun2Loose", 1);
-	//ski_tree->SetBranchAddress("HBHENoiseFilterResultRun2Loose", &HBHENoiseFilterResultRun2Loose);
-
-	ski_tree->SetBranchStatus("pprimaryVertexFilter", 1);
-	ski_tree->SetBranchAddress("pprimaryVertexFilter", &pprimaryVertexFilter);
-
-	/*ski_tree->SetBranchStatus("pBeamScrapingFilter", 1);
-	ski_tree->SetBranchAddress("pBeamScrapingFilter", &pBeamScrapingFilter);
-
-	ski_tree->SetBranchStatus("phfCoincFilter",1);
-        ski_tree->SetBranchAddress("phfCoincFilter",&phfCoincFilter);
-
-	ski_tree->SetBranchStatus("pVertexFilterCutdz1p0",1);
-	ski_tree->SetBranchAddress("pVertexFilterCutdz1p0",&pVertexFilterCutdz1p0);
+	read_trees(hlt_tree,is_MC);
 	
-	ski_tree->SetBranchStatus("pVertexFilterCutGplus",1);
-	ski_tree->SetBranchAddress("pVertexFilterCutGplus",&pVertexFilterCutGplus);
-
-	ski_tree->SetBranchStatus("pVertexFilterCutVtx1",1);
-	ski_tree->SetBranchAddress("pVertexFilterCutVtx1",&pVertexFilterCutVtx1);
-
-	*/
-       
-	jet_tree->SetBranchStatus("*", 0);
-
-	//trk tree information ****************************************************
-
-	trk_tree->SetBranchStatus("*",0);
-	const int nmax = 10000;
 	
-	Int_t nTrk=0;
-	//float trkPt[10000];
-	std::vector<Float_t>*    trkPt = nullptr;
-        std::vector<Float_t>* 	 trkPtError= nullptr;
-	std::vector<Float_t>*    trkEta= nullptr;
-	std::vector<Float_t>*    trkPhi= nullptr;
-	std::vector<Int_t>*      trkNHits= nullptr;
-	std::vector<Float_t>*    trkNormChi2= nullptr;
-	std::vector<Bool_t>*     highPurity= nullptr;
-	std::vector<Float_t>*    pfEnergy= nullptr;
-	std::vector<Float_t>*    pfEcal= nullptr;
-	std::vector<Float_t>*    pfHcal= nullptr;
-	std::vector<Float_t>*    trkDzErrAssociatedVtx= nullptr;
-	std::vector<Float_t>*    trkDxyErrAssociatedVtx= nullptr;
-        
-	trk_tree->SetBranchStatus("trkPt", 1);
-	trk_tree->SetBranchAddress("trkPt", &trkPt);    
-
-        trk_tree->SetBranchStatus("trkEta", 1);
-        trk_tree->SetBranchAddress("trkEta", &trkEta);
-
-	trk_tree->SetBranchStatus("trkPhi", 1);
-        trk_tree->SetBranchAddress("trkPhi", &trkPhi);
-
-	trk_tree->SetBranchStatus("nTrk", 1);
-        trk_tree->SetBranchAddress("nTrk", &nTrk);
-
-	trk_tree->SetBranchStatus("trkNormChi2", 1);
-        trk_tree->SetBranchAddress("trkNormChi2", &trkNormChi2);
-
-	trk_tree->SetBranchStatus("highPurity", 1);
-        trk_tree->SetBranchAddress("highPurity", &highPurity);
-
-	trk_tree->SetBranchStatus("trkPtError",1);
-	trk_tree->SetBranchAddress("trkPtError",&trkPtError);
-
-	trk_tree->SetBranchStatus("trkDzErrAssociatedVtx",1);
-        trk_tree->SetBranchAddress("trkDzErrAssociatedVtx",&trkDzErrAssociatedVtx);
-
-	trk_tree->SetBranchStatus("trkDxyErrAssociatedVtx",1);
-        trk_tree->SetBranchAddress("trkDxyErrAssociatedVtx",&trkDxyErrAssociatedVtx);
-	
-	hea_tree->GetEntry(i);
-	//return;
 	hlt_tree->GetEntry(i);
-	//	return;
-	ski_tree->GetEntry(i);
-	//jet_tree->GetEntry(i);
-	//return;
-	trk_tree->GetEntry(i);
-	//return;
+	
 	//Event Cuts ***************************************************************
 	if(vz <= -15. || vz >= 15.) continue; // vertex cut
 	          
@@ -247,18 +138,33 @@ void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t
 	    if(trk_dzerror > 3.0) continue;
 	    if(trk_dxyerror > 3.0) continue;
 
-	    //	    return;
 	    htrkpteta->Fill(trk_pt,trk_eta);
 	    hinvyield->Fill(trk_pt,invyield_wt);
-	    //	    return;
-
-	    //cout<<trk_pt<<"  "<<trk_eta<<" "<<endl;
 	    
-
-
-
 	  } // events loop end
 
+	//jet loop starts
+	int njets=(int) nref;
+	for (int j=0; j<njets; j++)
+	  {
+
+	    if(trackMax[j]/rawpt[j] < 0.05)continue; // jet cut for very low pT particles
+	    if(trackMax[j]/rawpt[j] > 0.95)continue; // jets where all the pT is taken by one track
+
+	    float jet_rawpt = rawpt[j];
+	    float jet_eta = jteta[j];
+	    float jet_phi = jtphi[j];
+
+	    if (abs(jet_eta)>2) continue;
+
+	    hjtpt->Fill(jet_rawpt);
+	    hjteta->Fill(jet_eta);
+	    hjtphi->Fill(jet_phi);
+	    
+	    
+	  }
+	
+	
       }		
 
     delete hea_tree;      
@@ -276,9 +182,10 @@ void Track_Analyzer(TString input_file, TString outputFileName,int is_MC,Float_t
     hZvtx->Write();
     htrkpteta->Write();
     hinvyield->Write();
-    //	return;
-      
-
+    
+    hjtpt->Write();
+    hjtphi->Write();
+    hjteta->Write();
   
     fout->Write();
     fout->Close();
